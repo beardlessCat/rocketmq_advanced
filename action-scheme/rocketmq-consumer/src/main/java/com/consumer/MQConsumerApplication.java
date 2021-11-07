c77ca9d97dbf3bb9bd78b37f4d936b46ee0e5b49
lyj <lyj@dareway.com.cn>
2021-11-07 21:28:44 +0800

    add：增加测试类

---
+++ action-scheme/rocketmq-consumer/src/main/java/com/consumer/MQConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MQConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MQConsumerApplication.class);
+    }
+
+
+}
---
+++ action-scheme/rocketmq-producer/src/test/java/com/producer/ProducerTest.java
@@ -0,0 +1,21 @@
+package com.producer;
+
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {MQProducerApplication.class})
+public class ProducerTest {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @Test
+    public void test1(){
+        rocketMQTemplate.convertAndSend("springboot-mq","hello springboot rocketmq");
+    }
+}
