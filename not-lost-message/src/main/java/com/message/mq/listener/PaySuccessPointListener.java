aba548cc579ea4ac7765b4e055b06402d659cd9a
lyj <948228970@qq.com>
2021-12-08 15:29:54 +0800

    add:增加消息消费ACK测试

--- not-lost-message/src/main/java/com/message/mq/listener/PaySuccessPointListener.java
+++ not-lost-message/src/main/java/com/message/mq/listener/PaySuccessPointListener.java
@@ -5,16 +5,21 @@ import com.message.entity.Order;
 import com.message.server.PointServer;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j
 @Component
 @RocketMQMessageListener(topic = "${mq.order.topic.success}",consumerGroup = "${mq.order.consumer.group.point}",messageModel = MessageModel.CLUSTERING )
 public class PaySuccessPointListener implements RocketMQListener<MessageExt> {
+    AtomicInteger atomicInteger = new AtomicInteger(0);
     @Autowired
     private PointServer pointServer ;
     @SneakyThrows
@@ -24,6 +29,14 @@ public class PaySuccessPointListener implements RocketMQListener<MessageExt> {
         log.info("收到订单支付成功消息{}",msgId);
         String body = new String(messageExt.getBody(), "UTF-8");
         Order order = JSON.parseObject(body, Order.class);
-        pointServer.distribute(order);
+        try {
+            log.info("积分消费者消费消息!");
+
+            //int x = 1/atomicInteger.getAndIncrement()-1 ; 异常处理
+            pointServer.distribute(order);
+        }catch (Exception e){
+            //抛出异常后，MQClient会返回ConsumeConcurrentlyStatus.RECONSUME_LATER进行重试，
+            throw e;
+        }
     }
 }
